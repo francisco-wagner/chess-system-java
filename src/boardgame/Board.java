f881dde65def5ad2ce0ec92346f6dee9f6d2d443
francisco-wagner <franciscowagner200@gmail.com>
2024-03-19 19:24:54 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -49,6 +49,19 @@ public class Board {
         piece.position = position;
     }
 
+    public Piece removePiece(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardException("Position not on the board");
+        }
+        else if (piece(position) == null) {
+            return null;
+        }
+        Piece aux = piece(position);
+        aux.position = null;
+        pieces[position.getRow()][position.getColumn()] = null;
+        return aux;
+    }
+
 
     private boolean positionExists(int row, int column) {
         return row >= 0 && row < rows && column >= 0 && column < columns;
@@ -60,7 +73,6 @@ public class Board {
 
     public boolean thereIsAPiece(Position position) {
         if (!positionExists(position)) {
-            System.out.println("Row: " + position.getRow() + " Column: " + position.getColumn());
             throw new BoardException("Position not on the board");
         }
         return piece(position) != null;
